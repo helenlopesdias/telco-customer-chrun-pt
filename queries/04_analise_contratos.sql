37b79d59a0ac4e8ac68fa37bc045310292056e91
Helen Dias <contatohelenlopesdias@gmail.com>
2026-07-17 19:15:10 -0300

    Create 04_analise_contratos.sql

---
+++ queries/04_analise_contratos.sql
@@ -0,0 +1,14 @@
+SELECT
+  Contract,
+  COUNT(customerID) AS total_customers,
+  SUM(CASE WHEN Churn = TRUE THEN 1 ELSE 0 END) AS total_churn,
+  ROUND(
+    100 * AVG(CASE WHEN Churn = TRUE THEN 1.0 ELSE 0.0 END), 
+    2
+  ) AS churn_rate_percentage,
+FROM 
+  `clear-aurora-469314-a0.Custumer_Churn.telco_customer_churn`
+GROUP BY 
+  Contract
+ORDER BY 
+  churn_rate_percentage DESC

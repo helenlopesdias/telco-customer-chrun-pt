5cba3acbd32372283277bd09838a012abd894baf
Helen Dias <contatohelenlopesdias@gmail.com>
2026-07-17 18:59:02 -0300

    Add SQL query for customer churn metrics
    This SQL query calculates various customer churn metrics based on customer profiles and behaviors, including average tenure for churned and active customers, and churn rates for single and married customers, as well as senior citizens.

---
+++ queries/02_kpis_saude_negocio.sql
@@ -0,0 +1,36 @@
+-- PERFIL DE CLIENTE E COMPORTAMENTO --
+
+SELECT
+-- Tenure médio de quem cancelou vs quem continua ativo:
+  ROUND(
+    AVG(CASE WHEN Churn = TRUE THEN tenure END), 2) AS avg_tenure_churn_yes,
+  ROUND(
+    AVG(CASE WHEN Churn = FALSE THEN tenure END), 2) AS avg_tenure_churn_no,  
+
+-- Taxa de cancelamento pessoas solteiras:
+  ROUND(
+    100 * SAFE_DIVIDE(
+      SUM(CASE WHEN Partner = FALSE AND Churn = TRUE THEN 1 ELSE 0 END),
+      SUM(CASE WHEN Partner = FALSE THEN 1 ELSE 0 END)
+    ),
+    2
+  ) AS churn_rate_no_partner,
+
+-- Taxa de cancelamento pessoas casadas:
+  ROUND(
+    100 * SAFE_DIVIDE(
+      SUM(CASE WHEN Partner = TRUE AND Churn = TRUE THEN 1 ELSE 0 END),
+      SUM(CASE WHEN Partner = TRUE THEN 1 ELSE 0 END)
+    ),
+    2
+  ) AS churn_rate_with_partner,
+
+-- Taxa de churn entre clientes idosos:
+  ROUND(
+    100 * SAFE_DIVIDE(
+      SUM(CASE WHEN SeniorCitizen = 1 AND Churn = TRUE THEN 1 ELSE 0 END),
+      SUM(CASE WHEN SeniorCitizen = 1 THEN 1 ELSE 0 END)
+    ),
+    2
+  ) AS churn_senior_citizen,
+FROM `clear-aurora-469314-a0.Custumer_Churn.telco_customer_churn`

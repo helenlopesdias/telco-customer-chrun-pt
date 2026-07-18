575d49fbf9b379e0e2c7032123a7f6172b939a43
Helen Dias <contatohelenlopesdias@gmail.com>
2026-07-18 18:51:50 -0300

    Add churn rate query for non-tech support customers
    Query calculates churn rates for customers without tech support.

---
+++ queries/09_churn_by_internet_servicechurn_by_internet_service.sql
@@ -0,0 +1,16 @@
+-- TAXAS DE CHURN ENTRE CLIENTES *NÃO* QUE CONTRATARAM SUPORTE TÉCNICO
+
+SELECT  
+  TechSupport,
+  COUNT(customerID) AS total_customers,
+  
+  -- Média de churn
+  ROUND(
+    AVG(CASE WHEN Churn = TRUE AND TechSupport = 'No' THEN 1 ELSE 0 END), 2
+  ) AS rate_not_oline_security_churn
+FROM 
+  `clear-aurora-469314-a0.Custumer_Churn.vw_telco_customer_churn_clean`
+GROUP BY
+  TechSupport
+ORDER BY 
+  TechSupport DESC

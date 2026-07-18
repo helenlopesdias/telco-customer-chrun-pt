e512697f91733eddd4c89bea56391d0282917da4
Helen Dias <contatohelenlopesdias@gmail.com>
2026-07-18 19:27:34 -0300

    Update churn analysis SQL query
    Added a comment regarding churn rates and modified the churn rate calculation to remove unnecessary multiplication.

--- queries/04_analise_contratos.sql
+++ queries/04_analise_contratos.sql
@@ -1,9 +1,13 @@
+-- TAXAS DE CHURN DE ACORDO COM TIPO DE CONTRATO
+
 SELECT
   Contract,
   COUNT(customerID) AS total_customers,
   SUM(CASE WHEN Churn = TRUE THEN 1 ELSE 0 END) AS total_churn,
+
+  -- Média de cancelamento
   ROUND(
-    100 * AVG(CASE WHEN Churn = TRUE THEN 1.0 ELSE 0.0 END), 
+    AVG(CASE WHEN Churn = TRUE THEN 1.0 ELSE 0.0 END), 
     2
   ) AS churn_rate_percentage,
 FROM 

053158718b432aa4fec224b9cbcbb256fafeb934
Helen Dias <contatohelenlopesdias@gmail.com>
2026-07-18 21:45:20 -0300

    Update 08_churn_by_internet_service.sql

--- queries/08_churn_by_internet_service.sql
+++ queries/08_churn_by_internet_service.sql
@@ -4,13 +4,14 @@ SELECT
   OnlineSecurity,
   COUNT(customerID) AS total_customers,
   
-  -- Média de cancelamento
+  -- Média de cancelamento real convertida para %
   ROUND(
-    AVG(CASE WHEN Churn = TRUE AND OnlineSecurity = 'No' THEN 1 ELSE 0 END), 2
-  ) AS rate_not_oline_security_churn
+    AVG(CASE WHEN Churn = TRUE
+  THEN 1.0 ELSE 0.0 END), 2
+    ) AS rate_online_security_churn
 FROM 
   `clear-aurora-469314-a0.Custumer_Churn.vw_telco_customer_churn_clean`
 GROUP BY
   OnlineSecurity
 ORDER BY 
-  OnlineSecurity DESC
+  rate_online_security_churn DESC;

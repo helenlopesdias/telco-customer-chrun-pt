c15a9659026e3c7b503e0d186f01a403ee71be04
Helen Dias <contatohelenlopesdias@gmail.com>
2026-07-23 00:02:08 -0300

    Change churn rate rounding precision to 4 decimals
    Updated the rounding precision for the churn rate calculation from 2 to 4 decimal places.

--- queries/09_churn_by_tech_suport.sql
+++ queries/09_churn_by_tech_suport.sql
@@ -5,7 +5,7 @@ SELECT
   
   -- Média de cancelamento real convertida para %
   ROUND(
-  AVG(CASE WHEN Churn = TRUE THEN 1 ELSE 0 END), 2
+  AVG(CASE WHEN Churn = TRUE THEN 1 ELSE 0 END), 4
   ) AS rate_tech_support_churn
 FROM 
   `clear-aurora-469314-a0.Custumer_Churn.vw_telco_customer_churn_clean`

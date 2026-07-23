9a5c4ba96ad4c402620bd448af3a2f2739df027a
Helen Dias <contatohelenlopesdias@gmail.com>
2026-07-22 23:02:00 -0300

    Update 06_analise_financeira.sql

--- queries/06_analise_financeira.sql
+++ queries/06_analise_financeira.sql
@@ -3,15 +3,22 @@
 SELECT
   PaymentMethod,
   COUNT(customerID) AS total_customers,
+
+-- Receita total por método de pagamento
   ROUND(
     SUM(MonthlyCharges), 2) AS total_monthly_revenue,
+
+-- Receita perdida por método de pagamento
+ROUND(
+  SUM(CASE WHEN Churn = TRUE THEN MonthlyCharges END), 2
+) AS monthly_revenue_lost,
 
 -- % do faturamento total da empresa
 ROUND(
   SAFE_DIVIDE(
     SUM(MonthlyCharges), 
     SUM(SUM(MonthlyCharges)) OVER()
-    ), 2) AS revenue_share_percentage
+    ), 4) AS revenue_share_percentage
 FROM
   `clear-aurora-469314-a0`.Custumer_Churn.telco_customer_churn
 GROUP BY

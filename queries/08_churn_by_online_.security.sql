0d986c30f8f4a05dd1381efcd4e02d7bd5ef3d6e
Helen Dias <contatohelenlopesdias@gmail.com>
2026-07-22 23:52:25 -0300

    Update 08_churn_by_online_.security.sql

--- queries/08_churn_by_online_.security.sql
+++ queries/08_churn_by_online_.security.sql
@@ -6,8 +6,7 @@ SELECT
   
   -- Média de cancelamento real convertida para %
   ROUND(
-    AVG(CASE WHEN Churn = TRUE
-  THEN 1.0 ELSE 0.0 END), 2
+    AVG(CASE WHEN Churn = TRUE THEN 1 ELSE 0 END), 4
     ) AS rate_online_security_churn
 FROM 
   `clear-aurora-469314-a0.Custumer_Churn.vw_telco_customer_churn_clean`

cddc33c13cc09a0f818bf54003ac9ed0c1f2a7de
Helen Dias <contatohelenlopesdias@gmail.com>
2026-07-23 23:26:23 -0300

    Fix churn rate calculation rounding precision

--- queries/05_impacto_formas_pagamento.sql
+++ queries/05_impacto_formas_pagamento.sql
@@ -13,7 +13,7 @@ SELECT
   -- Média de cancelamentos
   ROUND(
     AVG(CASE WHEN Churn = TRUE THEN 1 ELSE 0 END), 
-    2
+    4
   ) AS churn_rate_percentage,
 FROM
   `clear-aurora-469314-a0`.Custumer_Churn.telco_customer_churn
